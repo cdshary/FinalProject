156e49e7cff766400a1b17fc9d162e262fc5c7e1
cdshary <cdshary@yahoo.com>
2020-03-26 05:15:02 -0700

    push to view page

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,9 @@
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
+var button =d3.select("#button1");
+button.on("click", handleClick);
+function handleClick(){}
 
 (function($) {
 
